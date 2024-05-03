adbc521eccca1a10bd0cec0713bfd8e83dfb21d5
Agathisi-Imesh-Bandar <124579120+Imesh-Bandar@users.noreply.github.com>
2024-05-03 15:05:18 +0530

    add click the menu  btn and add resposive css for header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
  
+//  Get navbar query selector in to varible 
+ navbar=document.querySelector('.header .flex .navbar');
+
+// when i click the menu btn change the css class 'active'
+ document.querySelector('#menu-btn').onclick=()=>{
+    navbar.classList.toggle('active');
+ }
